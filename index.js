bb83367ddb7185510b5d65ee0e5fc3d549d4bdce
bayan <bayana.mhmd99@gmail.com>
2020-02-01 22:17:35 +0200

    not yet

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const server = require('./lib/server.js');
+const mongoose =require('mongoose');
+
+const MONGODB_URI = 'mongodb://localhost:27017/lab08' ;
+
+const mongooseOptions = {
+  useNewUrlParser: true ,
+  useCreateIndex: true ,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+};
+
+mongoose.connect(MONGODB_URI, mongooseOptions);
+
+server.start();
