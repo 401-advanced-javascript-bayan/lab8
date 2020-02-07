5ff7cdddcc490d568300849499540a1abf63df73
bayan <bayana.mhmd99@gmail.com>
2020-02-07 21:33:20 +0200

    update here

--- models/categories/categories-schema.js
+++ models/categories/categories-schema.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const categories = mongoose.Schema({
   name: {type : String , require : true},
   kids : {type : Number , require : true},
-  favourite : {type : Array , require : true },
+  favourites : {type : Array , require : true },
 });
 
 module.exports = mongoose.model('categories', categories);
